f860c92ee711a3097c0ead0c3291bb1832ac1212
Artem Lipodat <a.lipodat@mail.ru>
2023-02-15 16:30:26 +0400

    mobile

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './styles/styles.scss';
+import $ from "jquery";
+
+let toggle = document.querySelector('.menu input')
+$('#mobile_menu').on('click', () => {
+  console.log('aaa')
+})
+document.querySelector('body').addEventListener('click', () => {
+  toggle.checked = !toggle.checked
+})
